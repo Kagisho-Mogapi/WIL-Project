f7e8713ee8513f989fd02a5d8eae0b7b7d12e11c
edwardAlonzo <93587991+edwardAlonzo@users.noreply.github.com>
2021-11-09 20:30:53 +0200

    Create welcome_page.dart

---
+++ interstate_bus_services_app/lib/Pages/welcome_page.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.symmetric(horizontal: 50, vertical: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Text(
+                    'Welcome',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 40,
+                      fontWeight: FontWeight.normal,
+                      fontStyle: FontStyle.italic,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 100,
+                  ),
+                  Image.asset(
+                    'assets/images/Interstatelogo.jpg',
+                    alignment: Alignment.center,
+                  ),
+                  SizedBox(
+                    height: 60,
+                  ),
+                  Text(
+                    'Interstate Bus Line',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    'Making life easier for our commuters',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 12,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 60,
+                  ),
+                  MaterialButton(
+                    height: 50,
+                    minWidth: 200,
+                    color: Colors.red[700],
+                    onPressed: () {},
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  MaterialButton(
+                    height: 50,
+                    minWidth: 200,
+                    color: Colors.red[700],
+                    onPressed: () {},
+                    child: Text(
+                      'Sign In',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
