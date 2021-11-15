e1d910178763c6899df253f7b549b174e7c75d8a
edwardAlonzo <93587991+edwardAlonzo@users.noreply.github.com>
2021-11-15 11:55:41 +0200

    Message Page

---
+++ interstate_bus_services_app/lib/Pages/message_page.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:interstate_bus_services_app/widgets/message_card.dart';
+
+class MessagePage extends StatefulWidget {
+  const MessagePage({Key? key}) : super(key: key);
+
+  @override
+  _MessagePageState createState() => _MessagePageState();
+}
+
+class _MessagePageState extends State<MessagePage> {
+  late TextEditingController messageController;
+
+  @override
+  void initState() {
+    super.initState();
+    messageController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    messageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(       
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.grey.shade600, Colors.grey.shade200],
+          ),
+        ),
+        child: Stack(
+          children: <Widget>[
+            SafeArea(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CircleAvatar(
+                      radius: 70,
+                      backgroundImage:
+                          AssetImage('assets/images/BlankProfile.png'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Name Surname',
+                          style: TextStyle(fontSize: 22, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Divider(
+                    height: 15,
+                    thickness: 5,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Messages',
+                          style: TextStyle(
+                            fontSize: 24,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 8.0,
+                        vertical: 20,
+                      ),
+                      child: ListView.builder(
+                          itemCount: 2,
+                          itemBuilder: (context, index) {
+                            return MessageCard(
+                              messageToggleAction: (value) async {},
+                              deletaAction: () async {},
+                            );
+                          }),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
