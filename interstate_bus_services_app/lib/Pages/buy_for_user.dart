4ab7dd8d8878d7d3cbe274c75010226f86d2795f
edwardAlonzo <93587991+edwardAlonzo@users.noreply.github.com>
2021-11-20 22:25:42 +0200

    BuyForUser

---
+++ interstate_bus_services_app/lib/Pages/buy_for_user.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+class BuyForUser extends StatefulWidget {
+  const BuyForUser({Key? key}) : super(key: key);
+
+  @override
+  _BuyForUserState createState() => _BuyForUserState();
+}
+
+class _BuyForUserState extends State<BuyForUser> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: Stack(
+        children: [
+          Container(
+            constraints: BoxConstraints.expand(),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/Background1.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Opacity(
+            opacity: 0.85,
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [Colors.red, Colors.blue]),
+              ),
+            ),
+          ),
+          Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset(
+                      'assets/images/PurchaseDetail.png',
+                      alignment: Alignment.center,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: 44,
+                        ),
+                        Text(
+                          'Purchase Details',
+                          style: TextStyle(fontSize: 22, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    width: 300,
+                    padding: EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 2,
+                      ),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Recipient',
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    width: 300,
+                    padding: EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 2,
+                      ),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Enter Amount',
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.red[400],
+                        fixedSize: Size(300, 60),
+                      ),
+                      onPressed: () {},
+                      child: Text('Pay'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
